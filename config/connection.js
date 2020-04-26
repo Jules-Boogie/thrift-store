7d8ed25fa33b22913c82e3cae452e43e2856f38a
Juliet George <georgejuliet11@gmail.com>
2020-04-25 21:39:09 -0700

    final- connect database to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,24 @@
 // require mysql 
 var mysql = require("mysql");
 
+// //set up the connection to database
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Jule1995!",
+//   database: "boutique_db"
+// });
+
+
 //set up the connection to database
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Jule1995!",
-  database: "boutique_db"
+  user: "tlmvh5b1ho9c5fx0",
+  password: "wif2u3eddm8lhqxr",
+  database: "ji9cutxr6xmyceyq"
 });
-
-
-//set up the connection to database
-// var connection = mysql.createConnection({
-//   host: "jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//   port: 3306,
-//   user: "tlmvh5b1ho9c5fx0",
-//   password: "wif2u3eddm8lhqxr",
-//   database: "ji9cutxr6xmyceyq"
-// });
 // // Make connection.
 connection.connect(function(err) {
   if (err) {
